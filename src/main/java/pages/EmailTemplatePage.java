b2503c0138a651324dbb0ad973df515e789e02c6
AditiAtTier5 <adititier5@gmail.com>
2022-11-11 20:13:47 +0530

    send email done

--- src/main/java/pages/EmailTemplatePage.java
+++ src/main/java/pages/EmailTemplatePage.java
@@ -3,7 +3,7 @@ package pages;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
--- src/main/java/testcases/Runner.java
+++ src/main/java/testcases/Runner.java
@@ -4,9 +4,8 @@ import org.testng.annotations.DataProvider;
 import base.BaseClass;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = { "src/main/java/stepdefinitions/EmailTemplate.feature",
-		"src/main/java/stepdefinitions/SendEmail.feature" }, glue = "pages", plugin = {
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
+@CucumberOptions(features = "src/main/java/stepdefinitions/SendEmail.feature", glue = "pages", plugin = {
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, monochrome = true)
 public class Runner extends BaseClass {
 
 	@DataProvider(parallel = false)
